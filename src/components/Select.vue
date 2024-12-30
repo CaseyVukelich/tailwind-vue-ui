952483484582d9be9837906298e5d4a712890b51
Your Name <you@example.com>
2024-12-30 15:19:01 -0600

    added select component

---
+++ src/components/Select.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps(['options', 'selected'])
+defineEmits(['change'])
+</script>
+
+<template>
+  <select
+    @change="$emit('change', $event.target.value)"
+    class="bg-violet-400 focus:ring focus:ring-violet-600 active:shadow-xl text-cream-10 font-semivold rounded-full py-2 px-4"
+  >
+    <option
+      v-for="(opt, i) in options"
+      :key="i"
+      :value="opt.value"
+      :selected="i === selected"
+    >
+      {{opt.name}}
+    </option>
+  </select>
+</template>
